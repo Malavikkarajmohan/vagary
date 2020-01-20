2a9923f0629a83b7eab4dccd4a4a25839cbd400a
shaanzie <ishaanlagwankar@gmail.com>
2020-01-20 20:31:18 +0530

    Flask backend setip

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, jsonify, request, abort
+import json
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/packages")
+def packages():
+    return render_template("packages.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/single_blog")
+def single_blog():
+    return render_template("single-blog.html")
+
+@app.route("/top_place")
+def top_place():
+    return render_template("top_place.html")
+
+@app.route("/tour_details")
+def tour_details():
+    return render_template("tour_details.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
