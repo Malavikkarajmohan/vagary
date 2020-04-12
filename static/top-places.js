f44b3b8cbdce830cf2931385a478cc54a300409a
shaanzie <ishaanlagwankar@gmail.com>
2020-04-13 01:05:47 +0530

    Multi stage download works

--- static/top-places.js
+++ static/top-places.js
@@ -16,7 +16,7 @@
 //     }
 // }
 
-var obj = {
+var showdets = {
 
     xhr: new XMLHttpRequest(),
     showDetails : function(){
@@ -25,60 +25,85 @@ var obj = {
             type: "GET",
             url: '/recommend',
             success: function(data) {
-                
-                places = data['recommend'].split(";");
-                // first place
-                var place1= document.getElementById("place1");
-                var img1 =document.createElement("img");
-                // get the url from the db
-                img1.src = places[0].split(",")[1];
 
-                place1.appendChild(img1)
-                // // get country's name 
-                var ht1 = document.createElement("h3");
-                // assign country's name to ht1
-                ht1.innerHTML = places[0].split(",")[0];
-                var d1 = document.getElementById("d1");
-                d1.appendChild(ht1);
                 
-                var place2= document.getElementById("place2");
-                var img2 =document.createElement("img");
-                // get the url from the db
-                img2.src = places[1].split(",")[1];
 
-                place2.appendChild(img1)
-                // // get country's name 
-                var ht2 = document.createElement("h3");
-                // assign country's name to ht1
-                ht2.innerHTML = places[1].split(",")[0];
-                var d2 = document.getElementById("d1");
-                d2.appendChild(ht2);
                 
-                var place3= document.getElementById("place3");
-                var img3 =document.createElement("img");
-                // get the url from the db
-                img3.src = places[2].split(",")[3];
+                setTimeout(function() {
 
-                place3.appendChild(img3)
-                // // get country's name 
-                var ht3 = document.createElement("h3");
-                // assign country's name to ht3
-                ht3.innerHTML = places[2].split(",")[0];
-                var d3 = document.getElementById("d1");
-                d3.appendChild(ht3);
+                    places = data['recommend'].split(";");
+                    // console.log(places);
+                    // first place
+                    var place1= document.getElementById("place1");
+                    var img1 =document.createElement("img");
+                    // get the url from the db
+                    img1.src = places[0].split(",")[1];
+                    img1.style.height = '400px';
+                    place1.appendChild(img1)
+                    // // get country's name 
+                    var ht1 = document.createElement("h3");
+                    // assign country's name to ht1
+                    ht1.innerHTML = places[0].split(",")[0];
+                    console.log(places[0].split(",")[0]);
+                    var d1 = document.getElementById("d1");
+                    d1.appendChild(ht1);
+
+                    setTimeout(function() {
+                        var place2= document.getElementById("place2");
+                        var img2 =document.createElement("img");
+                        // get the url from the db
+                        // img2.src = places[1].split(",")[1];
+                        img2.src = places[1].split(",")[1];
+                        img2.style.height = '400px';
+                        place2.appendChild(img2)
+                        // // get country's name 
+                        var ht2 = document.createElement("h3");
+                        // assign country's name to ht1
+                        ht2.innerHTML = places[1].split(",")[0];
+                        var d2 = document.getElementById("d2");
+                        d2.appendChild(ht2);
+
+                        setTimeout(function() {
+                            var place3= document.getElementById("place3");
+                            var img3 =document.createElement("img");
+                            // get the url from the db
+                            // img3.src = places[2].split(",")[1];
+                            img3.src = places[0].split(",")[1];
+                            img3.style.height = '400px';
+                            place3.appendChild(img3)
+                            // // get country's name 
+                            var ht3 = document.createElement("h3");
+                            // assign country's name to ht3
+                            ht3.innerHTML = places[2].split(",")[0];
+                            var d3 = document.getElementById("d3");
+                            d3.appendChild(ht3);
+
+                            setTimeout(function() {
+                                // var place4= document.getElementById("place4");
+                                // var img4 =document.createElement("img");
+                                // // get the url from the db
+                                // // img4.src = places[3].split(",")[1];
+                                // img2.src = places[0].split(",")[1];
+
+                                // place4.appendChild(img4)
+                                // // // get country's name 
+                                // var ht4 = document.createElement("h3");
+                                // // assign country's name to ht4
+                                // ht4.innerHTML = places[3].split(",")[0];
+                                // var d4 = document.getElementById("d4");
+                                // d4.appendChild(ht4);
+                            }, 2000)
+                        
+                        }, 2000);
+
+                    }, 2000);
+
+                }, 2000);
                 
-                var place4= document.getElementById("place4");
-                var img4 =document.createElement("img");
-                // get the url from the db
-                img4.src = places[3].split(",")[1];
+                
 
-                place4.appendChild(img4)
-                // // get country's name 
-                var ht4 = document.createElement("h3");
-                // assign country's name to ht4
-                ht4.innerHTML = places[3].split(",")[0];
-                var d4 = document.getElementById("d1");
-                d4.appendChild(ht4);
+                
+                
                 
 
             }
