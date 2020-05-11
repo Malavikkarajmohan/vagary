6ac0d2b2a4c1b9761eeb184b56d4e779b3fccc75
shaanzie <ishaanlagwankar@gmail.com>
2020-05-11 14:25:06 +0530

    Added unit testing

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,5 +1,17 @@
-from vagary import create_app
+import os
+import tempfile
 
-def test_hello(client):
-    response = client.get('/hello')
-    assert response.data == b'Hello, World!'
+import pytest
+import vagary
+
+
+@pytest.fixture
+def client():
+    db_fd, vagary.app.config['DATABASE'] = tempfile.mkstemp()
+    vagary.app.config['TESTING'] = True
+    client = vagary.app.test_client()
+    yield client
+
+def test_main_page(client):
+    response = client.get('/hello', follow_redirects=True)
+    assert response.status_code == 200
